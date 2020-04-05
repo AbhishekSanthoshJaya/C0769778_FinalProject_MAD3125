516f605889e67d6976e2c93d2aca3bac9345a99d
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-04 21:01:58 -0400

    Changes in design

--- app/src/main/java/com/aby/c0769778_finalproject_mad3125/MainActivity.java
+++ app/src/main/java/com/aby/c0769778_finalproject_mad3125/MainActivity.java
@@ -4,12 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+    private String emailId, password;
+    private TextInputEditText edtEmailIdText;
+    private TextInputEditText edtPasswordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        edtEmailIdText = findViewById(R.id.edtEmailIdText);
+        edtPasswordText = findViewById(R.id.edtPasswordText);
     }
 }
