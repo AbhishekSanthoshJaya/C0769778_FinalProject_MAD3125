5159855945538c39337b187c4fcdcb5ec085a38b
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-10 17:22:54 -0400

    [Abhishek] - Added date picker

--- app/src/main/java/com/aby/c0769778_finalproject_mad3125/adapters/AddCustomerActivity.java
+++ app/src/main/java/com/aby/c0769778_finalproject_mad3125/adapters/AddCustomerActivity.java
@@ -2,11 +2,15 @@ package com.aby.c0769778_finalproject_mad3125.adapters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 
 import com.aby.c0769778_finalproject_mad3125.R;
@@ -16,6 +20,8 @@ import com.aby.c0769778_finalproject_mad3125.util.DataRepository;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,10 +48,10 @@ public class AddCustomerActivity extends AppCompatActivity {
     @BindView(R.id.edtPasswordText) TextInputEditText edtPasswordText;
     @BindView(R.id.edtLocationText) TextInputEditText edtLocationText;
 
-    @BindView(R.id.btnAdd)
-    Button btnAdd;
-    @BindView(R.id.btnClear)
-    Button btnClear;
+    @BindView(R.id.btnAdd) Button btnAdd;
+    @BindView(R.id.btnClear) Button btnClear;
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,14 +59,66 @@ public class AddCustomerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_customer);
         ButterKnife.bind(this);
 
+        addingDatePicker();
+
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fieldChecker();
             }
         });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearfields();
+            }
+        });
     }
 
+    private void addingDatePicker()
+    {
+        edtBirthDateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AddCustomerActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                String date ="";
+                month = month + 1;
+                String monthName = getMonthName(month);
+                if(day<10) {
+                    date = "0"+day + "-" + monthName + "-" + year;
+                }
+                else
+                {
+                    date = day + "-" + monthName + "-" + year;
+                }
+                edtBirthDateText.setText(date);
+            }
+        };
+    }
+
+    public static String getMonthName(int monthNumber){
+        String[] monthNames = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
+        return monthNames[monthNumber-1];
+    }
     public void fieldChecker()
     {
         boolean someFlag = false;
---
+++ app/src/main/java/com/aby/c0769778_finalproject_mad3125/util/HelperMethods.java
@@ -0,0 +1,26 @@
+package com.aby.c0769778_finalproject_mad3125.util;
+
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class HelperMethods {
+
+    private static HelperMethods repoObj = new HelperMethods();
+    public static HelperMethods getInstance() {
+        return repoObj;
+    }
+    private HelperMethods()
+    { }
+
+    public LocalDate stringToDate(String aDate)
+    {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd-MMM-yyyy");
+        return formatter.parseLocalDate(aDate);
+    }
+
+    public String doubleFormatter(Double d)
+    {
+        return String.format("%.2f", d);
+    }
+}
