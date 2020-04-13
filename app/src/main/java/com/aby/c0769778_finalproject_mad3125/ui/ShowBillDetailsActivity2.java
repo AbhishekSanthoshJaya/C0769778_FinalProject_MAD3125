77b6cf0620a4a9eb751961e9f4f705e1cf1fc72f
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-13 17:59:55 -0400

    [Abhishek] UI changes

--- app/src/main/java/com/aby/c0769778_finalproject_mad3125/ui/ShowBillDetailsActivity2.java
+++ app/src/main/java/com/aby/c0769778_finalproject_mad3125/ui/ShowBillDetailsActivity2.java
@@ -1,11 +1,15 @@
 package com.aby.c0769778_finalproject_mad3125.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.aby.c0769778_finalproject_mad3125.R;
 import com.aby.c0769778_finalproject_mad3125.adapters.BillsAdapter;
@@ -19,11 +23,15 @@ public class ShowBillDetailsActivity2 extends AppCompatActivity {
     private RecyclerView rvBillsList;
     private ArrayList billsArrayList;
     private BillsAdapter billsAdapter;
+    private ImageView imgAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_bill_details2);
+
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.setTitle("YOUR BILLS");
 
         Intent mIntent = getIntent();
         Customer customerObj = mIntent.getParcelableExtra("CustomerBills");
@@ -37,6 +45,14 @@ public class ShowBillDetailsActivity2 extends AppCompatActivity {
         rvBillsList.setLayoutManager(mLinearLayoutManager);
         rvBillsList.setAdapter(billsAdapter);
 
+        imgAddButton = findViewById(R.id.imgAddBill);
+        imgAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(ShowBillDetailsActivity2.this, AddNewBillActivity.class);
+                startActivity(mIntent);
+            }
+        });
 
     }
 }
